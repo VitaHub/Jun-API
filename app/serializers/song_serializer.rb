2668d3908ddec24641c00e29906cd87fb9ccc2cf
VitaHub <hubenko_vitalii@mail.ua>
2017-08-22 14:17:11 +0300

    SongsSerializer updated

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,3 +1,9 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :audio, :extension
+  attributes :id, :title, :author, :extension
+
+  def attributes(*args)
+    args = super(*args)
+    args[:audio_url] = object.audio.url
+    args
+  end
 end
