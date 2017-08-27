de622f96bee352c1a593faa80da3d6da815d2f7f
VitaHub <hubenko_vitalii@mail.ua>
2017-08-27 17:49:58 +0300

    audio/mp3 mime type added

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,9 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+MIME::Types.add(
+  MIME::Type.new('audio/mp3').tap do |type|
+    type.add_extensions 'mp3'
+  end
+)
